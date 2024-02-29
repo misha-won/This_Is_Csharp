96854c25274809058f3db1a44431a599afbe0339
gamjaaw <wsm0423@naver.com>
2024-02-29 15:20:33 +0900

    3.9.4 String Interpolation Chapter 03 Practice

---
+++ 03/31_StringInterpolation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace _31_StringInterpolation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "유지민";
+            int age = 23;
+            WriteLine($"{name, -10}, {age:D3}");
+
+            name = "김민정";
+            age = 30;
+            WriteLine($"{name}, {age, -10:D3}");
+
+            name = "김애리";
+            age = 17;
+            WriteLine($"{name}, {(age > 19 ? "성인" : "미성년자")}");
+
+            // 유지민       , 023
+            // 김민정, 030
+            // 김애리, 미성년자
+        }
+    }
+}
